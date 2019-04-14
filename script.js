f340687f2c1cd0e2eeb856456a15c975086f90f1
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-14 16:49:47 -0400

    rework js audio logic using jquery lib, touch up style

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-'use strict'
-/* global fetch */
-
-function query (selector) {
-  return document.querySelector(selector)
-}
-
-function queryAll (selector) {
-  return document.querySelectorAll(selector)
-}
-
-function getMusic (search) {
-  encodeURIComponent(search)
-  let promise = fetch(`https://itunes-api-proxy.glitch.me/search?term=${search}&amp;media=music`)
-    .then(function (response) {
-      if (!response.ok) {
-        throw Error(response.statusText)
-      }
-      return response.json()
-    })
-  return promise
-}
-
-function displayMusic (search) {
-  const resultDiv = query('#results')
-  resultDiv.innerHTML = ''
-  getMusic(search)
-    .then(function (result) {
-      for (let item of result.results) {
-        const trackDiv = document.createElement('div')
-        trackDiv.classList.add('track')
-        trackDiv.setAttribute('data-song-source', item.previewUrl)
-        let albumArt = document.createElement('img')
-        let title = document.createElement('p')
-        let header = document.createElement('h2')
-        albumArt.src = item.artworkUrl100
-        header.innerText = item.artistName
-        title.innerText = item.trackName
-        trackDiv.append(albumArt)
-        trackDiv.append(title)
-        trackDiv.append(header)
-        resultDiv.append(trackDiv)
-        console.log(title)
-      }
-    })
-}
-
-query('#search-bar').addEventListener('change', function (event) {
-  displayMusic(event.target.value)
+$(document).on('keypress', function (event) {
+  if (event.originalEvent.code === 'Enter') {
+    searchSongs()
+  }
 })
+$('.search button').on('click', searchSongs)
+
+function searchSongs () {
+  let inputContents = $('.search input').val()
+  let queryType = $("input[name*='query']:checked")
+
+  $.ajax({
+    url: `https://itunes-api-proxy.glitch.me/search`,
+    data: {
+      term: inputContents,
+      media: 'music',
+      entity: queryType.val()
+    },
+    dataType: 'json'
+  }).done(function (result) {
+    $('.results').empty()
+    for (let item of result.results) {
+      let track = document.createElement('div')
+      track.classList.add('track')
+      track.setAttribute('data-song', item.previewUrl)
+      let albumImage = document.createElement('img')
+      let heading = document.createElement('h2')
+      let songName = document.createElement('p')
+      albumImage.src = item.artworkUrl100
+      heading.innerText = item.artistName
+      songName.innerText = item.trackName
+      track.append(albumImage)
+      track.append(songName)
+      track.append(heading)
+      $('.results').append(track)
+      $(track).on('click', playSong)
+    }
+  }).fail(function (error, response) {
+    console.log('There was an error')
+    console.log(response)
+  })
+}
+
+let player = document.querySelector('.play audio')
+$(player).on('click', function () {
+  console.log('hello')
+})
+
+function playSong (event) {
+  player.src = event.delegateTarget.dataset.song
+  player.play()
+}
